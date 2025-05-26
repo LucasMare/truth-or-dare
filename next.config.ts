cd639e0710cdddb13b218a237c59b7a42b3bdd52
lucas.mare <lucas.mare.smart@gmail.com>
2025-05-26 18:37:18 +0300

    Restored local git functionality

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', 
+
+  //basePath: '/truth-or-dare', // Important for GitHub Pages
+  //assetPrefix: '/truth-or-dare'
+
 };
 
 export default nextConfig;
