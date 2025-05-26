79709f943d0462c4889f55cae2e61daefcbcde2a
lucas.mare <lucas.mare.smart@gmail.com>
2025-05-26 18:43:43 +0300

    Attempt #1 to make Github Pages work

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 import type { NextConfig } from "next";
 
+const isGithubPages = process.env.GITHUB_PAGES === 'true';
+
 const nextConfig: NextConfig = {
   output: 'export', 
-
-  //basePath: '/truth-or-dare', // Important for GitHub Pages
-  //assetPrefix: '/truth-or-dare'
-
+  basePath: isGithubPages ? '/truth-or-dare' : '',
+  assetPrefix: isGithubPages ? '/truth-or-dare/' : '',
 };
 
 export default nextConfig;
