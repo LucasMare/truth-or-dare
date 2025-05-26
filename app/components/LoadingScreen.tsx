acec73c6391f2cbcff4cc25b002c64aeb913acfb
lucas.mare <lucas.mare.smart@gmail.com>
2025-05-26 20:06:52 +0300

    Fixed Github Pages functionality

--- app/components/LoadingScreen.tsx
+++ app/components/LoadingScreen.tsx
@@ -5,7 +5,7 @@ import React from "react";
 export default function LoadingScreen() {
   return (
     <div className="fixed inset-0 bg-black flex items-center justify-center z-50">
-      <span className="text-white text-3xl font-semibold">Loading..</span>
+      <span className="text-white text-3xl font-semibold">Loading...</span>
     </div>
   );
 }
